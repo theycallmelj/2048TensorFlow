c68bdb00417f344e5f0c22acd6bfe8182e492a57
theycallmelj <43352450+theycallmelj@users.noreply.github.com>
2019-03-26 23:50:09 -0400

    fixed nerual network
    now network can generate values and loss function works

--- ai.py
+++ ai.py
@@ -2,7 +2,7 @@ import tensorflow as tf
 import board as b
 import numpy as np
 from numpy import array
-def NeuralNet():
+def NeuralNet(inpu):
      
         
  
@@ -13,11 +13,11 @@ def NeuralNet():
                 HiddenLayer(hiddenweights, i + 1)
         
         outputweights = tf.Variable(initial_value=tf.random_normal(shape=[20, 4],stddev = 0.4),dtype = 'float', name="Output")
-        hiddenLayers = tf.matmul(inputs, hiddenweights[0])
+        hiddenLayers = tf.matmul(inpu, hiddenweights[0])
         for i in range(199):
-                hiddenLayers = tf.matmul(hiddenLayers, hiddenweights[i+1])
-
-        output=tf.matmul(hiddenLayers, outputweights)
+               hiddenLayers = tf.sigmoid(tf.matmul(hiddenLayers, hiddenweights[i+1]))
+        #have to add some bias neurons
+        output=tf.sigmoid(tf.matmul(hiddenLayers, outputweights))
         return output
 
 def HiddenLayer(hiddenweights, i):
@@ -26,6 +26,8 @@ def HiddenLayer(hiddenweights, i):
 
 
 def train(inputs, boar, nnin):
+    
+        
         boardSliced = []
         topTile = b.maxTile(boar)
         for x in range(4):
@@ -36,32 +38,42 @@ def train(inputs, boar, nnin):
    
         qMax = b.add(b.qMultiply(b.qMax(boar), discountRate), b.reward(boar))
      
-        
-        
-        learningRate = .001
-        optimizer = tf.train.GradientDescentOptimizer(learning_rate=learningRate)
+        y = tf.placeholder(tf.float32, shape=[None,4],name="y" )
+    
+        learningRate = .01
+  
         #left is  [1, 0, 0, 0]
         #right is [0, 1, 0, 0]
         #down is  [0, 0, 1, 0]
         #up is    [0, 0, 0, 1] 
         
-        l = tf.square(qMax - nnin)
+        l = tf.squared_difference(nnin*b.maxTile(boar), y)
+        l = tf.reduce_mean(l)
+        optimizer = tf.train.GradientDescentOptimizer(learning_rate=learningRate).minimize(l)
         #l is the distance between maxQ and expected Q
         
         #I in theory want the diffenerce in rates to be minized due to convergence
        
-        train_op = optimizer.minimize(l)
+        #train_op = optimizer.minimize(l)
         
 
         
-        model = tf.global_variables_initializer()
+ 
         with tf.Session() as session:
-                session.run(model, {inputs : array([boardSliced])})
+                tf.global_variables_initializer().run()
+                print("Aloha")
+                i = 0
+                while( i < 5):
+                        qMax = b.add(b.qMultiply(b.qMax(boar), discountRate), b.reward(boar))
+                        p = session.run([l], feed_dict={inputs : [boardSliced], y : [qMax]})
+                        b.doDown(boar)
+                        i +=1
+                        print(p)
                 
 inputs = tf.placeholder(tf.float32, shape=[None, 16],name="Input" )        
 bor = b.newBoard()
 b.addNewValue(bor)
-nnin = NeuralNet()
+nnin = NeuralNet(inputs)
 train(inputs, bor, nnin)
 
 
